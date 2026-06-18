0e4c8fc9d30e50232bf02568454082b0a699c85e
Ben Helliwell <ben.helliwell@electronet.co.nz>
2026-06-19 09:31:59 +1200

    State and transition

--- terminal_dnd.py
+++ terminal_dnd.py
@@ -1,6 +1,13 @@
 from yaml import safe_load_all, YAMLError
 
 
+class State():
+    def __init__(self, name: str, command: str, allowed_states: list[str]) -> None:
+        self.name = name
+        self.command = command
+        self.allowed_states = allowed_states
+
+
 class Character():
     def __init__(self, config: dict) -> None:
         self.name = config["Name"]
@@ -35,11 +42,42 @@ def yaml_read(yaml_file: str, section: str) -> dict:
         except YAMLError as e:
             raise SystemError(f"Error reading yaml file {e}")
 
+
+def trim_input(usr_input: str) -> str:
+    return usr_input.upper()[:3]
+
+
+def transition_states(allowed_states: list[State], usr_input: str, current_state: State) -> State:
+    """Transition to a new state if the user commands it. Otherwise 'transition' to the current state.
+    
+    Args:
+        allowed_states (list[State]): subset of all states that we're allowed to transition to from our current state
+        usr_input (str): user input in format 'XXX'
+        current_state (State): the state we're currently in
+    Returns:
+        State: the new state to transition to, or our current state if the user input doesn't match any of our allowed commands
+    """
+    for state in allowed_states:
+        if usr_input == state.command:
+            return state
+    # Return the current state if the user input doesn't match an allowed transition command
+    return current_state
+
+
 def main() -> None:
     my_char = Character(yaml_read("char_sheet.yaml", "Character"))
-    print(my_char)
-    print(my_char.name)
-    print(my_char.attributes)
+    states = {"main": State("main", "BAC", ["spells"]),
+              "spells": State("spells", "SPE", ["main"])}
+
+    # Mainloop
+    usr_input = ""
+    state = states["main"]
+    while usr_input != "EXI":
+        usr_input = trim_input(input(">>>"))
+        state = transition_states(allowed_states=[states[name] for name in states.keys() if name in state.allowed_states],
+                                  usr_input=usr_input,
+                                  current_state=state)
+        print(state.name)
 
 
 if __name__ == "__main__":
